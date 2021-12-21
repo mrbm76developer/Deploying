fdcf07c175bf38b1edf35039a37ab38e3a8a0035
Mohammad Reza <mohammadrezabehzadimoqadam@gmail.com>
2021-12-21 11:44:30 +0330

    Lazu loading

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { Route, Switch, Redirect } from "react-router-dom";
+import React, { Suspense } from "react";
+import AllQuotes from "./pages/AllQuotes";
+import NotFound from "./pages/NotFound";
+import Layout from "./components/layout/Layout";
+
+const NewQuote = React.lazy(() => import("./pages/NewQuote"));
+const QuoteDetail = React.lazy(() => import("./pages/QuoteDetail"));
+function App() {
+  return (
+    <Layout>
+      <Suspense fallback={<div className="centered">Loading...</div>}>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/quotes" />
+          </Route>
+          <Route path="/quotes" exact>
+            <AllQuotes />
+          </Route>
+          <Route path="/quotes/:quoteId">
+            <QuoteDetail />
+          </Route>
+          <Route path="/new-quote">
+            <NewQuote />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Suspense>
+    </Layout>
+  );
+}
+
+export default App;
